d93909bc561654b8e9315f5142391fe3170a2ccb
sarinaa-m <sarin.moayedi@gmail.com>
2021-05-01 23:54:57 +0430

    js add

--- site.js
+++ site.js
@@ -1,15 +1,47 @@
-var videoPlayer = document.getElementById('videoPlayer');
-var videoLen = document.getElementById('videoLen');
+// time and date HOME PAGE
+var myDate = new Date();
+document.getElementById('dateTime').innerHTML = myDate.toLocaleDateString('fa');
+document.getElementById('clock').innerHTML = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+// CONTACT PAGE
 
-function videoPlay() {
-    videoPlayer.play();
-}
 
-function videoPause() {
-    videoPlayer.pause();
-}
+function checkInfo() {
+    var inputName = document.getElementById('inputName');
+    var location = document.getElementById('inputLocation');
+    var emaill = document.getElementById('inputEmail');
+    var phoneNumber = document.getElementById('inputPhone');
 
-// setInterval(function() {
-//     videoLen.innerHTML = videoPlayer.currentTime + '/' + videoPlayer.duration;
+    var result = true;
+    var infoElements = document.getElementsByClassName('invalid');
+    for (const element of infoElements) {
+        infoElements.innerHTML = '';
+    }
 
-// }, 100);
+    if (inputName.value === '') {
+        inputName.nextElementSibling.innerHTML = "لطفا اطلاعات خود را وارد کنید";
+        result = false;
+
+    }
+    if (location.value === '') {
+        location.nextElementSibling.innerHTML = "لطفا اطلاعات خود را وارد کنید";
+        result = false;
+
+    }
+    if (emaill.value === '') {
+        emaill.nextElementSibling.innerHTML = "لطفا ایمیل خود را وارد کنید";
+        result = false;
+    }
+    if (phoneNumber.value === '') {
+        phoneNumber.nextElementSibling.innerHTML = "لطفا شماره تماس خود را وارد کنید";
+        result = false;
+    }
+
+    if (inputName.value !== '' && emaill.value !== '' && location.value !== '' && phoneNumber.value != '') {
+        alert('پیام شما با موفقیت ثبت شد');
+    }
+    inputName.value = '';
+    location.value = '';
+    emaill.value = '';
+    phoneNumber.value = '';
+
+};
