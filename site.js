a5b4b6fef8379ca1b79714552844e893aab6da8b
sarinaa-m <sarin.moayedi@gmail.com>
2021-05-03 15:12:48 +0430

    js add

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 // time and date HOME PAGE
-var myDate = new Date();
-document.getElementById('dateTime').innerHTML = myDate.toLocaleDateString('fa');
-document.getElementById('clock').innerHTML = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+// var myDate = new Date();
+// document.getElementById('dateTime').innerHTML = myDate.toLocaleDateString('fa');
+// document.getElementById('clock').innerHTML = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
 
 // PAGE SCROLL
 window.onscroll = function() {
@@ -63,3 +63,32 @@ function checkInfo() {
     phoneNumber.value = '';
 
 };
+// ABOUT US PAGE
+(function() {
+
+    'use strict';
+    var items = document.querySelectorAll(".timeline li");
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
+    }
+
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+
+})();
